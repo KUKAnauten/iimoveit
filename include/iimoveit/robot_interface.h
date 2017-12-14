d826b67ef0f66c5c54dd5860f20667938487af64
Marcus Ebner von Eschenbach <herrlauch@gmail.com>
2017-12-14 12:12:51 +0100

    Add API documentation

--- include/iimoveit/robot_interface.h
+++ include/iimoveit/robot_interface.h
@@ -36,7 +36,6 @@
 
 
 //TODO publish trajectory, publish pose (cart/joint), start action, publish text
-//TODO documentation (doxygen/rosdoc_lite)
 
 #ifndef IIMOVEIT_ROBOT_INTERFACE_H_
 #define IIMOVEIT_ROBOT_INTERFACE_H_
@@ -54,31 +53,59 @@ namespace rvt = rviz_visual_tools;
  */
 class RobotInterface {
   public:
+  /**
+   * Constructs a new RobotInterface with the given parameters
+   * @param node_handle The NodeHandle used to publish and subscribe to topics.
+   * @param planning_group The planning group/joint model group to plan for.
+   * @param base_frame Common base for all visual markers.
+   */
   RobotInterface(ros::NodeHandle* node_handle, const std::string& planning_group, const std::string& base_frame);
 
+  /**
+   * Plans a motion to a given endeffector pose. It will show the planned trajectory in RViz and, if approvalRequired
+   * is set to true, will ask for confirmation before moving to the goal.
+   * @param target_pose The pose goal.
+   * @param pose_name The name of the pose that will be shown in RViz.
+   * @param approvalRequired If set to true, it will only move after confirmation in RViz.
+   */
   virtual void planAndMove(geometry_msgs::Pose& target_pose, const std::string& pose_name, bool approvalRequired);
 
+  /**
+   * Overloaded planAndMove(), where approvalRequired is set to true.
+   */
   virtual void planAndMove(geometry_msgs::Pose& target_pose, const std::string& pose_name);
 
+  /**
+   * Overloaded planAndMove(), where pose_name is set to 'given pose'.
+   */
   virtual void planAndMove(geometry_msgs::Pose& target_pose, bool approvalRequired);
 
+  /**
+   * Overloaded planAndMove(), where approvalRequired is set to true and pose_name is set to 'given pose'.
+   */
   virtual void planAndMove(geometry_msgs::Pose& target_pose);
 
+  /**
+   * Waits for the user to click 'Next' in RViz.
+   */
   virtual void waitForApproval();
 
 protected:
-  ros::NodeHandle* node_handle_;
-  ros::Publisher trajectory_publisher_;
-  const std::string PLANNING_GROUP_;
-  moveit::planning_interface::MoveGroupInterface move_group_;
-  moveit::planning_interface::PlanningSceneInterface planning_scene_interface_;
-  const robot_state::JointModelGroup *joint_model_group_;
-  moveit::planning_interface::MoveGroupInterface::Plan movement_plan_;
-  moveit_visual_tools::MoveItVisualTools visual_tools_;
-  Eigen::Affine3d text_pose_;
-  std::vector<std::string> joint_names_;
-  robot_state::RobotState robot_state_;
+  ros::NodeHandle* node_handle_; /**< The NodeHandle used to publish or subscribe messages. */
+  ros::Publisher trajectory_publisher_; /**< The publisher to publish trajectory messages to the command topic of the controller. */
+  const std::string PLANNING_GROUP_; /**< The name of the planning group/joint model group to use. */
+  moveit::planning_interface::MoveGroupInterface move_group_; /**< The MoveGroupInterface to use MoveIt!. */
+  moveit::planning_interface::PlanningSceneInterface planning_scene_interface_; /**< The PlanningSceneInterface to represent the planning scene. */
+  const robot_state::JointModelGroup *joint_model_group_; /**< The actual JointModelGroup instance. */
+  moveit::planning_interface::MoveGroupInterface::Plan movement_plan_; /**< This is where a MoveIt! plan will be stored. */
+  moveit_visual_tools::MoveItVisualTools visual_tools_; /**< Tools used to visualize in RViz. */
+  Eigen::Affine3d text_pose_; /**< The position of the status text, usually above the robot's head. */
+  std::vector<std::string> joint_names_; /**< A vector of joint names. */
+  robot_state::RobotState robot_state_; /**< This is where the current robot state is stored. */
 
+  /**
+   * Gets the current robot state and stores it.
+   */
   void updateRobotState();
 };
 
